3f666b9efbf15cbd5199b6c92c9e8ec09b9aecb3
Maimer <nesnl23@yahoo.com>
2014-06-06 11:55:43 -0400

    Added search field, and ability to add entries into the database

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'pry'
 
 require_relative 'models/contact'
 
@@ -13,6 +14,22 @@ get '/' do
   @total = Contact.all.size
   @contacts = Contact.all.limit(2).offset((@page * 2) - 2)
   erb :index
+end
+
+post '/' do
+  @contacts = Contact.where(first_name: params[:search])
+  @total = @contacts.size
+  erb :index
+end
+
+post '/add' do
+  @firstname = params[:first_name]
+  @lastname = params[:last_name]
+  @phonenumber = params[:phone_number]
+  Contact.find_or_create_by(first_name: @firstname, last_name:
+    @lastname, phone_number: @phonenumber)
+
+  redirect '/'
 end
 
 get '/contacts/:id' do
