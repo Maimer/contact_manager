44ed47036e02f89f1c1da895cf6e4ce26f987c27
Maimer <nesnl23@yahoo.com>
2014-06-06 11:35:51 -0400

    Added individual contact pages and pagination

--- app.rb
+++ app.rb
@@ -5,6 +5,17 @@ require 'sinatra/activerecord'
 require_relative 'models/contact'
 
 get '/' do
-  @contacts = Contact.all
+  if params[:page] != nil
+    @page = params[:page].to_i
+  else
+    @page = 1
+  end
+  @total = Contact.all.size
+  @contacts = Contact.all.limit(2).offset((@page * 2) - 2)
   erb :index
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :show
 end
